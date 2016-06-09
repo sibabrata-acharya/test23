2ffeb0bc258c2e86365b851b4bce12fa52ca2d9a
TMall <kasyap.aditya223@gmail.com>
2016-06-09 15:09:49 +0530

    We have uploaded your required codebase

---
+++ junit_test/server.js
@@ -0,0 +1,3 @@
+var http = require(‘http’);
+http.createServer(function(request, response) { response.writeHead(200, {“Content-Type”: “text/plain”}); response.end(“Hello from Cloud Foundry!\n”);
+}).listen(process.env.VCAP_APP_PORT);
